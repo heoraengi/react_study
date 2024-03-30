bb73b31e36a005c1dc933afd371056b74d5610f0
heoraengi <heo.raeng.i@gmail.com>
2024-03-30 13:14:51 +0900

    add function

--- ex04/src/components/Register.jsx
+++ ex04/src/components/Register.jsx
@@ -2,7 +2,8 @@ import React, { useRef, useState } from 'react'
 import {Button, Form} from 'react-bootstrap';
 
 const Register = ({onRegister1}) => {
-    const newCode = useRef(103)
+    const newCode = useRef(103);
+    const refName = useRef(null);
     const [product, setProduct] = useState({
         code : newCode.current,
         name : '',
@@ -27,17 +28,24 @@ const Register = ({onRegister1}) => {
                     name : '', 
                     price : '',
                     qnt : ''
-                })
+                });
+                refName.current.focus();
             }
+        }
+    }
+
+    const onKeyDown = (e) => {
+        if(e.key==='Enter'){
+            onClick();
         }
     }
 
     return (
         <div className='mb-5'>
-            <Form.Control value={code} name='2' onChange={onChange} placeholder='상품코드' className='mb-2'/>
-            <Form.Control value={name} name='name' onChange={onChange}  placeholder='상품이름' className='mb-2'/>
+            <Form.Control value={code} name='2' placeholder='상품코드' className='mb-2' readOnly/>
+            <Form.Control ref={refName} value={name} name='name' onChange={onChange}  placeholder='상품이름' className='mb-2'/>
             <Form.Control value={price} name='price' onChange={onChange}  placeholder='상품가격' className='mb-2'/>
-            <Form.Control value={qnt} name='qnt' onChange={onChange} placeholder='상품수량' className='mb-2'/>
+            <Form.Control onKeyDown={onKeyDown} value={qnt} name='qnt' onChange={onChange} placeholder='상품수량' className='mb-2'/>
             <Button onClick={onClick}>상품등록</Button>
         </div>
     )
