6fd1f0ba7fc27ef0fcdf4f2f8a964d511e795e44
heoraengi <heo.raeng.i@gmail.com>
2024-04-20 11:20:09 +0900

    add ex07

---
+++ ex07/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Locals from './components/Locals';
+
+function App() {
+    return (
+        <div className="App">
+            <Locals/>
+        </div>
+    );
+}
+
+export default App;
