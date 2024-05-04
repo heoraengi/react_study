bfb1bb1ad90bdb25957072db4aaaf1efbb987013
heoraengi <heo.raeng.i@gmail.com>
2024-05-04 14:08:42 +0900

    Add bbs project

---
+++ bbs/src/firebaseinit.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeRKSZeksitY7ETUqr4Tl0XJKzqQ_9DTg",
+  authDomain: "inha-2513e.firebaseapp.com",
+  projectId: "inha-2513e",
+  storageBucket: "inha-2513e.appspot.com",
+  messagingSenderId: "761742299345",
+  appId: "1:761742299345:web:2ef612ca1f21e43df475f0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
